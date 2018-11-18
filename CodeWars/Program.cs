36a5cde53743d23516734cb1654856f735fe0b75
Yurrko <yurrko@yandex.ru>
2018-11-18 12:51:25 +0300

    Parse HTML/CSS Colors

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -8,29 +8,72 @@ namespace CodeWars
     {
         static void Main( string[] args )
         {
-            //Console.WriteLine(
-            //    Kata.QueueTime( new[] { 1, 2, 3, 4 }, 1 )
-            //);
+            var colorDic = new Dictionary<string, string>();
+            colorDic.Add( "limegreen", "32cd32" );
+            var colorParser = new HtmlColorParser( colorDic );
 
             Console.WriteLine(
-                Kata.QueueTime( new int[] { 2, 2, 3, 3, 4, 4 }, 2 )
-                );
+                colorParser.Parse( "LimeGreen" )
+            );
 
             Console.ReadLine();
         }
     }
 
-    public class Kata
+    class HtmlColorParser
     {
-        public static long QueueTime( int[] customers, int n )
-        {
-            var registers = new List<int>( Enumerable.Repeat( 0, n ) );
+        private readonly IDictionary<string, string> presetColors;
 
-            foreach ( int cust in customers )
-            {
-                registers[registers.IndexOf( registers.Min() )] += cust;
-            }
-            return registers.Max();
+        public HtmlColorParser( IDictionary<string, string> presetColors )
+        {
+            this.presetColors = presetColors;
         }
+
+        public RGB Parse( string color )
+        {
+            string colorToParse;
+            color = color.ToLower();
+            if ( color[0].Equals( '#' ) )
+            {
+                colorToParse = color.Length == 4 ? $"{color[1]}{color[1]}{color[2]}{color[2]}{color[3]}{color[3]}" : color.Remove( 0, 1 );
+            }
+            else if ( presetColors.ContainsKey( color ) )
+            {
+                presetColors.TryGetValue( color, out colorToParse );
+                colorToParse = colorToParse.Remove(0, 1);
+            }
+            else
+                colorToParse = "000000";
+
+            byte parseToNum( string col )
+            {
+                return (byte) (parseToDigit(col[0]) * 16 + parseToDigit(col[1]));
+            }
+
+            byte parseToDigit( char ch )
+            {
+                if (ch >= '0' && ch <= '9')
+                    return (byte) (ch - '0');
+                if (ch >= 'a' && ch <= 'f')
+                    return (byte) (ch - 'a' + 10);
+                return 0;
+            }
+
+            return new RGB( parseToNum( colorToParse.Substring( 0, 2 ) ),
+                            parseToNum( colorToParse.Substring( 2, 2 ) ),
+                            parseToNum( colorToParse.Substring( 4, 2 ) )
+            );
+        }
+    }
+
+    struct RGB
+    {
+        public byte _r, _g, _b;
+        public RGB( byte r, byte g, byte b )
+        {
+            (_r, _g, _b) = (r, g, b);
+        }
+
+        public override string ToString() => $"R={_r} G={_g} B={_b}";
     }
 }
